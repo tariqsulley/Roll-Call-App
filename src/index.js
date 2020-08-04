738485848fcceb2c221b26eac8e0cbc34041b7f4
tariqsulley <32623579+tariqsulley@users.noreply.github.com>
2020-08-04 20:14:59 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Component } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Login from './Login.jsx';
+import Logiin from './Login.jsx';
 
 class App extends Component {
   render() {
