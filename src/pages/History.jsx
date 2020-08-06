bc69641093ea8864dde3bc89c4da2ecf5c29524d
tariqsulley <32623579+tariqsulley@users.noreply.github.com>
2020-08-06 03:05:20 +0000

    Update History.jsx

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -11,19 +11,6 @@ const styles = theme => ({
     display: "none"
   }
 });
-/* 
-[[[[{"name":"Prince Asiedu","house":"9","class":"1D2","tableData":{"id":2}}],[{"name":"Kalikrates","house":"9","class":"1H","tableData":{"id":5}}]]],[[[{"name":"Prince Asiedu","house":"9","class":"1D2","tableData":{"id":2}}],[{"name":"Kalikrates","house":"9","class":"1H","tableData":{"id":5}}]]],[[[{"name":"Jeslord Thompson","house":"9","class":"2G","tableData":{"id":0}},{"name":"Michael Norman","house":"9","class":"3B2","tableData":{"id":1}},{"name":"Prince Asiedu","house":"9","class":"1D2","tableData":{"id":2}},{"name":"Ronald Eyeson","house":"9","class":"3D3","tableData":{"id":3}}],[{"name":"Ernest Essien","house":"9","class":"2F","tableData":{"id":4}},{"name":"Kalikrates","house":"9","class":"1H","tableData":{"id":5}},{"name":"Michael Sungnuma","house":"9","class":"2D3","tableData":{"id":6}},{"name":"Alidu Wonzooya","house":"9","class":"2E","tableData":{"id":7}},{"name":"Shadrach Achambaka","house":"9","class":"2G","tableData":{"id":8}},{"name":"Fidel Taylor","house":"9","class":"2P","tableData":{"id":9}},{"name":"Emmanuel Buabeng","house":"9","class":"2G","tableData":{"id":10}},{"name":"Clement Davour","house":"9","class":"2G","tableData":{"id":11}},{"name":"Kingsley Buadi","house":"9","class":"3N","tableData":{"id":12}},{"name":"Lommo","house":"9","class":"3H","tableData":{"id":13}}]]],[[[{"name":"Michael Norman","house":"9","class":"3B2","tableData":{"id":1}}],[{"name":"Ronald Eyeson","house":"9","class":"3D3","tableData":{"id":3}},{"name":"Kingsley Buadi","house":"9","class":"3N","tableData":{"id":12}},{"name":"Lommo","house":"9","class":"3H","tableData":{"id":13}}]]]]
-{Completed_Calls.map(item =>(
-        (item.map(obj => (obj.map(stud => (stud.map(fin => <p>{fin.name}</p>))))))))}
-        
-            <div>   
-         {Completed_Calls.map(item =>{
-          return(
-            <div>
-              <h1>{item}</h1>
-              {item.map(obj => (obj.map(stud => (stud.map(fin => <p>{fin.name}</p>
-
-      </div>*/
 
 const Completed_Calls = JSON.parse(localStorage.getItem('completed'))
 
@@ -31,8 +18,10 @@ class HistoryPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: []
+      history: [],
+      current: []
       }
+      this.view = this.view.bind(this)
   }
   
   componentDidMount(){
@@ -41,43 +30,49 @@ class HistoryPage extends Component {
       })
       console.log(this.state.history)
     }
-  
+  view(stud){
+   alert('Hi' + stud.name)
+  }
 
   render() {
+    if(Completed_Calls !== null){
     return ( 
       <div className ="HistoryBody">   
         {Completed_Calls.map((item) =>{
           return(
-
             <div className="MainArray">
              {item.map((obj) => {
             return(
               <div className = "Completed_RollCall">
                 {obj.map((stud) => {
                 return(
-                  <div className ="Absent_Present">
-                   {stud.map((fin) =>
-                 <div className="Names"> {fin.name}  </div>  )}
+                  <div className ="Absent_Present" >
+                    {stud.map((fin) =>
+                      <div className="Names"> 
+                      <HistoryCard name={fin.name} Class = {fin.class}/>  
+                      </div> 
+                    )}
+                <p onClick ={this.view}>Present: {stud.length}</p>
+                <button onClick={()=>alert("Hello ")} > View </button>
                   </div>
                  )})}
+                 
               </div>
                  )})}
-                 
             </div> 
-            
+
           )})}
       </div>
-    )}}
-       
-     /*
-     else{
-       return(
-         <div>
-           <h1> No RollCall's Conducted Yet </h1>
-         </div>
-       )
-     }*/
+    )} 
+    
+    else{
+      return(
+        <div>
+          <h1> No RollCall's Conducted Yet </h1>
+        </div>
+      )
+    } 
+  } 
+}
     
 export default withStyles(styles)(HistoryPage);
-
-
