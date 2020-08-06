5a8ab08115a1883c8b294b457305e0c4e3180690
tariqsulley <32623579+tariqsulley@users.noreply.github.com>
2020-08-06 03:06:01 +0000

    Update HistoryCard.jsx

--- src/pages/HistoryCard.jsx
+++ src/pages/HistoryCard.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-const HistoryCard = ({name,present_num,absent_num,present_studs,absent_studs})=>{
+import './HistoryCard Styles.css'
+const HistoryCard = ({name,Class})=>{
     return(
         <div className="EditCard">
             <div className="EditBox">
-            <h1> {name} </h1>
-            <h2> {present_num}</h2>
-            <h2> {absent_num} </h2>
-            <p> {present_studs} </p>
-            <p> {absent_studs} </p>
+            <p iD ="Names"> {name} </p>
+            <p iD = "Class"> {Class} </p>
             </div>
         </div>
     )
