2deb37443c1b57ed089f3923b7d9c72b71ac3262
tariqsulley <32623579+tariqsulley@users.noreply.github.com>
2020-08-07 14:55:23 +0000

    Update Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,15 +3,15 @@ import TextField from '@material-ui/core/TextField';
 import './Settings Styles.css';
 
 class settings extends Component{
-constructor(props){
-  super(props)
-  this.state ={
-    password: '',
-    newpassword:'',
-    confirmNew:''
-  }
-this.LogOut = this.LogOut.bind(this)
-this.existingPassword = this.existingPassword.bind(this)
+  constructor(props){
+    super(props)
+    this.state ={
+      password: '',
+      newpassword:'',
+      confirmNew:''
+    }
+    this.LogOut = this.LogOut.bind(this)
+    this.existingPassword = this.existingPassword.bind(this)
 }
 
 LogOut(){
@@ -19,18 +19,20 @@ LogOut(){
   localStorage.setItem('login', true)
   window.location.reload()
 }
+
 passwordChange(){
   /*const password = localStorage.getItem('password')*/
-
 }
+
 existingPassword(event){
   this.setState({
     password: event.target.value
   })
 }
-applychanges(){
 
+applychanges(){
 }
+
 render(){
   return(
     <div className ="Background">
@@ -49,18 +51,23 @@ render(){
     <TextField iD = "TF1" variant ="outlined" label ="Existing passsword"  value={this.state.password} onChange={this.existingPassword} type="password"/>
     <TextField iD = "TF1" variant ="outlined" label ="New Password" type ="password"/>
     <TextField iD = "TF1" variant ="outlined" label ="Confirm New Password" type="password"/>
-    </div>
+    </div>  
 
     <div className ="Control_btns">
     <button  className="Apply_btn"> Apply Changes </button>
     <button className="LogOut_btn" onClick={this.LogOut}> Log Out </button>
     </div>
+    <div className = "Actions">
+      <h1> Students Upgrade</h1>
+      <p> Please do not click this button unless the form 3's have completed</p>
+      <button> Move Students</button>
+    </div>
 
     </div>
     </div>
     </div>
-  )
-}
+    )
+  }
 }
 
 export default (settings);
