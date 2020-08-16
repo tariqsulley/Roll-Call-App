fa0437f4ea27ff35b9645683dfed0f3378888415
tariqsulley <32623579+tariqsulley@users.noreply.github.com>
2020-08-16 05:14:11 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
+import Chart from "react-google-charts";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   button: {
@@ -10,7 +11,8 @@ const styles = theme => ({
     display: "none"
   }
 });
-// Add Functional to catch a critical critical bug here
+
+// Add Function to catch a critical critical bug here
 //all places in code where I get item from local storage 
 //and instanlty use it's value will crash on a different pc
 //because there's nothing intially in their local storage
@@ -18,7 +20,7 @@ const students = JSON.parse(localStorage.getItem('names'))
 const total_students = students.length
 const form_3s = students.filter(student => student.class[0] === "3").length
 const form_2s = students.filter(student => student.class[0] === "2").length
-const form_1s =  students.filter(student => student.class[0] === "1").length
+const form_1s =  students.filter(student => student.class[0] === "1").length 
 const Home = props => {
   return (
     <div>
@@ -29,10 +31,43 @@ const Home = props => {
         <p> Total Number Of Form 2 Students: </p>
         <p> Total Number Of Form 1 Students: </p>
       </div>
+      <div style={{ display: 'flex', maxWidth: 900}}>
+      <Chart
+  width={'500px'}
+  height={'300px'}
+  chartType="PieChart"
+  loader={<div><CircularProgress/></div>}
+  data={[
+    ['Grade', 'Number Of Students'],
+    ['Form 3 ', form_3s],
+    ['Form 2', form_2s],
+    ['Form 1', form_1s]
+  ]}
+  options={{
+    title: 'Proportion Of Students In The House',
+  }}
+/>
+  </div>
+  <Chart
+  width={1000}
+  height={350}
+  chartType="Calendar"
+  loader={<div>Loading Chart</div>}
+  data={[
+    [{ type: 'date', id: 'Date' }, { type: 'number', id: 'Won/Loss' }],
+    [new Date(2020,1,25), 37],
+    [new Date(2020,3,16), 25],
+    [new Date(2020, 8, 16), 58],
+    [new Date(2020,8,17), 70]
+  ]}
+  options={{
+    title: 'Club Meeting Attendance',
+  }}
+  rootProps={{ 'data-testid': '1' }}
+/>
     </div>
   );
 };
-
 
 
 export default withStyles(styles)(Home);
